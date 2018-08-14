5013c67f18e70f82f348e990b68d2f76e298a6c1
keyeh <keyeh@calpoly.edu>
2018-08-14 14:40:12 -0700

    No pointer on cards zero state

--- src/Card.js
+++ src/Card.js
@@ -12,6 +12,7 @@ export default (props) => {
                 src={getCardUrl(props.card)}
                 alt={`${props.card.value} of ${props.card.suit}s`}
                 onClick={() => props.onClick(props.id)}
+                style={{ cursor: "pointer" }}
             />
         );
     } else {
