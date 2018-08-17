cbf40d1118f4f5db8eabf2d2ebc5ccf343983006
keyeh <keyeh@calpoly.edu>
2018-08-16 17:18:48 -0700

    Add credit animation and win sounds

--- src/components/CreditContainer.js
+++ src/components/CreditContainer.js
@@ -1,9 +1,56 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
+import CountUp from "react-countup";
+import { playWinSound } from "../sounds";
 class CreditContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentAmount: props.credits
+        };
+    }
+
+    updateCurrentAmount(amount) {
+        this.setState({
+            currentAmount: amount
+        });
+    }
+
+    getAnimationDuration() {
+        if (this.state.currentAmount < this.props.credits) {
+            let delta = this.props.credits - this.state.currentAmount;
+            if (delta <= 20) {
+                playWinSound("1");
+                return 0.65;
+            }
+            if (delta <= 250) {
+                playWinSound("2");
+                return 1.2;
+            }
+            return 5; //full house
+        } else {
+            return 0.000001;
+        }
+    }
+
     render() {
-        return <span style={{ textAlign: "right" }}>CREDIT {this.props.credits}</span>;
+        return (
+            <CountUp
+                start={this.state.currentAmount}
+                end={this.props.credits}
+                duration={this.getAnimationDuration()}
+                delay={0}
+                useEasing={false}
+                onEnd={() => this.updateCurrentAmount(this.props.credits)}
+                prefix="CREDIT "
+            >
+                {({ countUpRef }) => (
+                    <div style={{ textAlign: "right" }}>
+                        <span ref={countUpRef} />
+                    </div>
+                )}
+            </CountUp>
+        );
     }
 }
 
